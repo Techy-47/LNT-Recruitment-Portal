216686a9484b392d7cd61cb6418912f37bf80ae6
Techy-47 <adityarajcse001@gmail.com>
2026-07-14 14:29:52 +0530

    Configure database connection using environment variables

--- src/main/java/com/lnt/util/DBConnection.java
+++ src/main/java/com/lnt/util/DBConnection.java
@@ -7,24 +7,42 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/recruitment_portal?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    private static final String HOST = System.getenv().getOrDefault("DB_HOST", "localhost");
+    private static final String PORT = System.getenv().getOrDefault("DB_PORT", "3306");
+    private static final String DATABASE = System.getenv().getOrDefault("DB_NAME", "recruitment_portal");
+
     private static final String USER = System.getenv().getOrDefault("DB_USER", "root");
+
     private static final String PASS = System.getenv().getOrDefault("DB_PASS", "root123");
+
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
+            + "?useSSL=false"
+            + "&allowPublicKeyRetrieval=true"
+            + "&serverTimezone=UTC";
+
     private static final int MAX_POOL_SIZE = Integer.parseInt(System.getenv().getOrDefault("DB_POOL_SIZE", "10"));
-    private static final long CONNECTION_TIMEOUT_MS = Long.parseLong(System.getenv().getOrDefault("DB_CONNECTION_TIMEOUT_MS", "30000"));
+
+    private static final long CONNECTION_TIMEOUT_MS = Long
+            .parseLong(System.getenv().getOrDefault("DB_CONNECTION_TIMEOUT_MS", "30000"));
 
     private static final HikariDataSource DATA_SOURCE = buildDataSource();
 
     private static HikariDataSource buildDataSource() {
+
         HikariConfig config = new HikariConfig();
+
         config.setJdbcUrl(URL);
         config.setUsername(USER);
         config.setPassword(PASS);
+
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
         config.setMaximumPoolSize(MAX_POOL_SIZE);
         config.setConnectionTimeout(CONNECTION_TIMEOUT_MS);
+
         config.setPoolName("recruitment-portal-pool");
-        config.setLeakDetectionThreshold(60000);
+
         return new HikariDataSource(config);
     }
 
